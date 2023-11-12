a15921504988e88aef33c3d261c3c72424d5daed
Joefrey Mahusay <joefrey.mahusay@gmail.com>
2023-11-12 20:37:47 +0800

    add connect js

---
+++ tx-connect.js
@@ -0,0 +1,22 @@
+(function () {
+  swiperConnectTestimonial();
+
+  var swiperConnectTestimonial = function () {
+    const swiper = new Swiper(".swiperTestimonial", {
+      // Optional parameters
+      //direction: 'vertical',
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+        el: ".swiper-pagination",
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  };
+})();
