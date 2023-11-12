6ba2ce02e64fd16f3c2a362db73733b98b10a52a
Joefrey Mahusay <joefrey.mahusay@gmail.com>
2023-11-12 19:34:38 +0800

    initial commit

--- tx-index.js
+++ tx-index.js
@@ -94,6 +94,60 @@
         yPercent: 45,
         ease: "power4.inOut",
         force3D: true,
+        onComplete: () => {
+          // const can = document.querySelector(".can > img");
+          console.log("Animation completed spaceship.");
+
+          const randomX = random(10, 20);
+          const randomY = random(20, 30);
+          const randomDelay = random(0, 1);
+          const randomTime = random(3, 5);
+          const randomTime2 = random(5, 10);
+          const randomAngle = random(8, 12);
+
+          TweenLite.set(pillSpaceship, {
+            x: randomX(-1),
+            y: randomX(1),
+            rotation: randomAngle(-1),
+          });
+
+          moveX(pillSpaceship, 1);
+          moveY(pillSpaceship, -1);
+          rotate(pillSpaceship, 1);
+
+          function rotate(target, direction) {
+            TweenLite.to(target, randomTime2(), {
+              rotation: randomAngle(direction),
+              // delay: randomDelay(),
+              ease: Sine.easeInOut,
+              onComplete: rotate,
+              onCompleteParams: [target, direction * -1],
+            });
+          }
+
+          function moveX(target, direction) {
+            TweenLite.to(target, randomTime(), {
+              x: randomX(direction),
+              ease: Sine.easeInOut,
+              onComplete: moveX,
+              onCompleteParams: [target, direction * -1],
+            });
+          }
+
+          function moveY(target, direction) {
+            TweenLite.to(target, randomTime(), {
+              y: randomY(direction),
+              ease: Sine.easeInOut,
+              onComplete: moveY,
+              onCompleteParams: [target, direction * -1],
+            });
+          }
+
+          function random(min, max) {
+            const delta = max - min;
+            return (direction = 1) => (min + delta * Math.random()) * direction;
+          }
+        },
       },
       "-=.5"
     );
