f19548e5c3f415e14484a7be8a083a2e98d7fccf
Joefrey Mahusay <joefreymahusay@Joefreys-MacBook-Pro.local>
2023-11-13 08:56:16 +0800

    Initial commit

--- tx-connect.js
+++ tx-connect.js
@@ -2,21 +2,22 @@
   swiperConnectTestimonial();
 
   function swiperConnectTestimonial() {
-    console.log("connect testimonial");
-    const swiper = new Swiper(".swiperTestimonial", {
-      // Optional parameters
-      //direction: 'vertical',
-      loop: true,
-
-      // If we need pagination
-      pagination: {
-        el: ".swiper-pagination",
-      },
-
-      // Navigation arrows
+    var swiper = new Swiper(".swiperTestimonial", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      speed: 700,
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        // renderBullet: function (index, className) {
+        //   return (
+        //     '<span class="' + className + '">' + (index + 1) + "</span>"
+        //   );
+        // },
       },
     });
   }
