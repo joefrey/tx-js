e637547f238c683c5d74cb658e0d0f7f5f3c6f56
Joefrey Mahusay <joefreymahusay@Joefreys-MacBook-Pro.local>
2023-11-13 19:20:58 +0800

    Initial commit

--- tx-advance.js
+++ tx-advance.js
@@ -37,6 +37,9 @@
         opacity: 1,
         ease: "power4.inOut",
         force3D: true,
+        onComplete: () => {
+          pillSpaceshipFloating(astronaut);
+        },
       },
       "+=3"
     );
@@ -108,4 +111,49 @@
       },
     });
   }
+
+  var pillSpaceshipFloating = function (el) {
+    const randomX = random(10, 20);
+    const randomY = random(20, 30);
+    const randomDelay = random(0, 1);
+    const randomTime = random(3, 5);
+    const randomTime2 = random(5, 10);
+    const randomAngle = random(8, 12);
+
+    moveX(el, 1);
+    moveY(el, -1);
+    rotate(el, 0.3);
+
+    function rotate(target, direction) {
+      TweenLite.to(target, randomTime2(), {
+        rotation: randomAngle(direction),
+        ease: Sine.easeInOut,
+        onComplete: rotate,
+        onCompleteParams: [target, direction * -1],
+      });
+    }
+
+    function moveX(target, direction) {
+      TweenLite.to(target, randomTime(), {
+        x: randomX(direction),
+        ease: Sine.easeInOut,
+        onComplete: moveX,
+        onCompleteParams: [target, direction * -1],
+      });
+    }
+
+    function moveY(target, direction) {
+      TweenLite.to(target, randomTime(), {
+        y: randomY(direction),
+        ease: Sine.easeInOut,
+        onComplete: moveY,
+        onCompleteParams: [target, direction * -1],
+      });
+    }
+
+    function random(min, max) {
+      const delta = max - min;
+      return (direction = 1) => (min + delta * Math.random()) * direction;
+    }
+  };
 })();
